fbfbcaa68db58f3e3368a0a8529d02e8df88a76f
Nate Watkins <natewatkinsca@gmail.com>
2024-01-19 11:13:56 -0800

    ran npm i

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import FullMovie from "./movie/FullMovie";
 import TheaterList from "./theaters/TheaterList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { Client } from 'rollbar-react-native'
-const rollbar = new Client('29853e81cef944b2addfdec3c59e0f31')
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '82f8477add8a4ec5b89bd4ebb481f600',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
 function App() {
